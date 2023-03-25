2534e0db0f3629a630a6730f328fac27be4d5196
LimHaeryong <nabi4622@snu.ac.kr>
2023-03-25 13:57:58 +0900

    kitti to measurement

--- object_tracking/include/modules/Measurement.h
+++ object_tracking/include/modules/Measurement.h
@@ -3,44 +3,66 @@
 
 #include <cmath>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
+
+struct Kitti_Object
+{
+    std::string type;
+    double truncated;
+    double occluded;
+    double alpha;
+    double left, top, right, bottom; // 2D bbox
+    double height, width, length;
+    double loc_x, loc_y, loc_z; // camera coordinates
+    double rot_y;
+    double score;
+};
+
+struct Kitti_Calib
+{
+    Eigen::Matrix<double, 3, 4> P0, P1, P2, P3;
+    Eigen::Matrix<double, 3, 3> R0_rect;
+    Eigen::Matrix<double, 3, 4> velo_to_cam;
+    Eigen::Matrix<double, 3, 4> imu_to_velo;
+};
+
+void load_kitti_label(const std::string &label_path, std::vector<Kitti_Object> &objs);
+Kitti_Calib load_kitti_calib(const std::string &calib_path);
 
 class Measurement
 {
 public:
-  Measurement();
-  virtual ~Measurement();
+    Measurement();
+    Measurement(int frame_cnt, const Kitti_Object &obj, const Kitti_Calib &calib);
+    virtual ~Measurement();
 
-  Eigen::VectorXd get_z() const;
-  Eigen::MatrixXd get_R() const;
-  Eigen::VectorXd get_hx(const Eigen::VectorXd& x) const;
-  Eigen::MatrixXd get_H(const Eigen::VectorXd& x) const;
-  Eigen::MatrixXd get_cam_to_veh() const;
-  double get_t() const;
+    Eigen::VectorXd get_z() const;
+    Eigen::MatrixXd get_R() const;
+    Eigen::VectorXd get_hx(const Eigen::VectorXd &x) const;
+    Eigen::MatrixXd get_H(const Eigen::VectorXd &x) const;
+    Eigen::MatrixXd get_cam_to_veh() const;
+    double get_t() const;
 
-  bool in_fov(const Eigen::VectorXd& x) const;
-
-  void print();
-
-  Eigen::VectorXd z_;  
-  Eigen::MatrixXd cam_to_veh_;
-  Eigen::MatrixXd veh_to_cam_;
-  Eigen::MatrixXd R_;
-
-  std::pair<double, double> fov_;
-
-  double t_;
-
-  uint class_num_;
-  double width_;
-  double length_;
-  double height_;
-  double yaw_;
-
+    void print();
 
 private:
+    Eigen::VectorXd z_;
+    Eigen::MatrixXd cam_to_veh_;
+    Eigen::MatrixXd veh_to_cam_;
+    Eigen::MatrixXd R_;
 
+    double t_;
+
+    std::string type_;
+    double width_;
+    double length_;
+    double height_;
+    double yaw_;
 };
 
-#endif  // MEASUREMENT_H_
+#endif // MEASUREMENT_H_
