86d1ef415b4f348aeaa315d7f3e63b916c54aa5a
hanjiho97 <hanjiho97@gist.ac.kr>
2023-03-28 00:04:13 +0900

    Update Projection class(add corners2 and change size)

--- result_viewer/include/result_viewer/Projection.h
+++ result_viewer/include/result_viewer/Projection.h
@@ -1,7 +1,6 @@
 #ifndef PROJECTION_H
 #define PROJECTION_H
 
-#include <iostream>
 #include <vector>
 #include <cmath>
 #include "Eigen/Dense"
@@ -20,24 +19,27 @@ public:
   std::vector<cv::Point> get_2D_corners();
 
 private:
-  Eigen::Matrix<double, 3, 8> corners_3D_;
-  Eigen::Matrix<double, 3, 8> corners_2D_;
+  Eigen::Matrix<double, 3, 14> corners1_3D_;
+  Eigen::Matrix<double, 3, 14> corners2_3D_;
+  Eigen::Matrix<double, 3, 14> corners1_2D_;
+  Eigen::Matrix<double, 3, 14> corners2_2D_;
   Eigen::Matrix<double, 3, 4> P2_;
   Attributes attributes_;
 
   void set_3D_corners( 
-    Eigen::Matrix<double, 3, 8>& corners_3D,
+    Eigen::Matrix<double, 3, 14>& corners1_3D,
+    Eigen::Matrix<double, 3, 14>& corners2_3D,
     const Attributes& attributes);
   bool rotate_3D_corners(
-    Eigen::Matrix<double, 3, 8>& corners_3D,
+    Eigen::Matrix<double, 3, 14>& corners_3D,
     const double rot_y);
   bool move_3D_corners_to_world(
-    Eigen::Matrix<double, 3, 8>& corners_3D,
+    Eigen::Matrix<double, 3, 14>& corners_3D,
     const Attributes& attributes);
   bool project_3D_to_2D(
-    const Eigen::Matrix<double, 3, 8>& corners_3D,
+    const Eigen::Matrix<double, 3, 14>& corners_3D,
     const Eigen::Matrix<double, 3, 4>& P2,
-    Eigen::Matrix<double, 3, 8>& corners_2D);
+    Eigen::Matrix<double, 3, 14>& corners_2D);
 };
 
 #endif  //
--- result_viewer/src/result_viewer/Projection.cpp
+++ result_viewer/src/result_viewer/Projection.cpp
@@ -5,23 +5,31 @@ Projection::Projection() {}
 Projection::~Projection() {}
 
 void Projection::set_3D_corners( 
-    Eigen::Matrix<double, 3, 8>& corners_3D,
+    Eigen::Matrix<double, 3, 14>& corners1_3D,
+    Eigen::Matrix<double, 3, 14>& corners2_3D,
     const Attributes& attributes)
 {
   double height = attributes.height;
   double width = attributes.width;
   double length = attributes.length;
-  corners_3D.row(0) << -length/2, length/2, length/2, length/2, 
-                        length/2, -length/2, -length/2, -length/2;
-  corners_3D.row(1) << -width, -width, 0, 0, 
-                       -width, -width, 0, 0;
-  corners_3D.row(2) << -height/2, -height/2, -height/2, height/2, 
-                        height/2, height/2, height/2, -height/2;
+  corners1_3D.row(0) << -length/2, -length/2, -length/2, -length/2, -length/2, -length/2, length/2, 
+                         length/2, -length/2, length/2, length/2, -length/2, length/2, length/2;
+  corners1_3D.row(1) << -width, -width, 0, 0, 0, -width, -width, 
+                        0, -width, -width, -width, -width, -width, 0;
+  corners1_3D.row(2) << -height/2, height/2, height/2, -height/2, height/2, height/2, height/2, 
+                         height/2, -height/2, -height/2, height/2, height/2, height/2, height/2;
+                        
+  corners2_3D.row(0) << length/2, length/2, length/2, length/2, -length/2, -length/2, length/2,
+                        length/2, -length/2, length/2, length/2, -length/2, length/2, length/2;
+  corners2_3D.row(1) << -width, -width, 0, 0, 0, -width, -width, 
+                        0, 0, 0, 0, 0, 0, -width;
+  corners2_3D.row(2) << -height/2, height/2, height/2, -height/2, -height/2, -height/2, -height/2, 
+                        -height/2, -height/2, -height/2, height/2, height/2, -height/2, -height/2;
   return;
 }
 
 bool Projection::rotate_3D_corners(
-  Eigen::Matrix<double, 3, 8>& corners_3D,
+  Eigen::Matrix<double, 3, 14>& corners_3D,
   const double rot_y)
 {
   Eigen::Matrix<double, 3, 3> rotation_matrix;
@@ -33,12 +41,12 @@ bool Projection::rotate_3D_corners(
 }
 
 bool Projection::move_3D_corners_to_world(
-Eigen::Matrix<double, 3, 8>& corners_3D,
-const Attributes& attributes)
+  Eigen::Matrix<double, 3, 14>& corners_3D,
+  const Attributes& attributes)
 {
   Eigen::Vector3d xyz;
   xyz << attributes.loc_x, attributes.loc_y, attributes.loc_z;
-  for(uint8_t col=0; col<8; ++col)
+  for(uint8_t col=0; col<14; ++col)
   {
     corners_3D.col(col) += xyz;
   }
@@ -46,12 +54,12 @@ const Attributes& attributes)
 }
 
 bool Projection::project_3D_to_2D(
-  const Eigen::Matrix<double, 3, 8>& corners_3D,
+  const Eigen::Matrix<double, 3, 14>& corners_3D,
   const Eigen::Matrix<double, 3, 4>& P2,
-  Eigen::Matrix<double, 3, 8>& corners_2D)
+  Eigen::Matrix<double, 3, 14>& corners_2D)
 {
-  Eigen::Matrix<double, 4, 8> corners = Eigen::Matrix<double, 4, 8>::Constant(1);
-  corners.block(0, 0, 3, 8) << corners_3D;
+  Eigen::Matrix<double, 4, 14> corners = Eigen::Matrix<double, 4, 14>::Constant(1);
+  corners.block(0, 0, 3, 14) << corners_3D;
   corners_2D = P2 * corners;
   return 0;
 }
@@ -69,16 +77,22 @@ std::vector<cv::Point> Projection::get_2D_corners()
 {
   std::vector<cv::Point> points;
   cv::Point point;
-  set_3D_corners(corners_3D_, attributes_);
-  rotate_3D_corners(corners_3D_, attributes_.rot_y);
-  move_3D_corners_to_world(corners_3D_, attributes_);
-  project_3D_to_2D(corners_3D_, P2_, corners_2D_);
-  for (uint8_t col=0; col<8; ++col)
+  set_3D_corners(corners1_3D_, corners2_3D_, attributes_);
+  rotate_3D_corners(corners1_3D_, attributes_.rot_y);
+  rotate_3D_corners(corners2_3D_, attributes_.rot_y);
+  move_3D_corners_to_world(corners1_3D_, attributes_);
+  move_3D_corners_to_world(corners2_3D_, attributes_);
+  project_3D_to_2D(corners1_3D_, P2_, corners1_2D_);
+  project_3D_to_2D(corners2_3D_, P2_, corners2_2D_);
+  for (uint8_t col=0; col<14; ++col)
   {
-    point.x = static_cast<uint16_t>(corners_2D_(0, col) / corners_2D_(2, col));
-    point.y = static_cast<uint16_t>(corners_2D_(1, col) / corners_2D_(2, col));
+    point.x = static_cast<uint16_t>(corners1_2D_(0, col) / corners1_2D_(2, col));
+    point.y = static_cast<uint16_t>(corners1_2D_(1, col) / corners1_2D_(2, col));
+    points.push_back(point);
+
+    point.x = static_cast<uint16_t>(corners2_2D_(0, col) / corners2_2D_(2, col));
+    point.y = static_cast<uint16_t>(corners2_2D_(1, col) / corners2_2D_(2, col));
     points.push_back(point);
   }
-  std::cout << points[7] << std::endl;
   return points;
 }
