56a9275cdf78d3b4f7363952c02d258e80de0583
hanjiho97 <hanjiho97@gist.ac.kr>
2023-03-27 10:36:29 +0900

    add base Dataloader class

---
+++ result_viewer/include/result_viewer/Dataloader.h
@@ -0,0 +1,71 @@
+#ifndef DATALOADER_H_
+#define DATALOADER_H_
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "Eigen/Dense"
+#include "opencv2/opencv.hpp"
+
+namespace kitti
+{
+struct Label
+{
+  std::string type;
+  double truncated;
+  double occluded;
+  double alpha;
+  double left, top, right, bottom;  // 2D bbox
+  double height, width, length;
+  double loc_x, loc_y, loc_z;  // camera coordinates
+  double rot_y;
+  double score;
+};
+
+struct Calibration
+{
+  Eigen::Matrix<double, 3, 4> P0, P1, P2, P3;
+  Eigen::Matrix<double, 3, 3> R0_rect;
+  Eigen::Matrix<double, 3, 4> velo_to_cam;
+  Eigen::Matrix<double, 3, 4> imu_to_velo;
+};
+
+struct Data
+{
+  Calibration calibration;
+  cv::Mat image;
+  std::vector<kitti::Label> labels;
+};
+} //namespace Kitti
+
+class Dataloader
+{
+public:
+  Dataloader();
+  virtual ~Dataloader();
+
+  bool load_kitti_calibration(
+    const std::string& calibration_path, 
+    kitti::Calibration& calibration_data);
+  bool load_kitti_image(
+    const std::string& image_path,
+    cv::Mat& image);
+  bool load_kitti_label(
+    const std::string& label_path,
+    std::vector<kitti::Label>& labels);
+
+  kitti::Data get_kitti_data(uint16_t frame_id);
+
+private:
+  uint8_t string_length_;
+  uint16_t frame_id_;
+  std::string kitti_root_path_;
+  std::string calibration_path_;
+  std::string image_path_;
+  std::string label_path_;
+};
+
+#endif  // DATALOADER_H_
---
+++ result_viewer/src/result_viewer/Dataloader.cpp
@@ -0,0 +1,169 @@
+#include "result_viewer/Dataloader.h"
+
+Dataloader::Dataloader() 
+{
+  string_length_ = 10;
+  frame_id_ = 0;
+  kitti_root_path_ = "../kitti_data";
+  calibration_path_ = "/calib/";
+  image_path_ = "/image_2/";
+  label_path_ = "/label_2/";
+}
+
+Dataloader::~Dataloader() {}
+
+bool Dataloader::load_kitti_calibration(
+  const std::string& calibration_path,
+  kitti::Calibration& calibration_data)
+{
+  std::ifstream ifs(calibration_path);
+  std::string line;
+  double value;
+  if (!ifs.is_open())
+  {
+    std::cout << "cannot open calib_path : " << calibration_path << std::endl;
+  }
+
+  std::getline(ifs, line);
+  std::stringstream ss(line.substr(4));
+  for (int row = 0; row < 3; ++row)
+  {
+    for (int col = 0; col < 4; ++col)
+    {
+      ss >> value;
+      calibration_data.P0(row, col) = value;
+    }
+  }
+
+  std::getline(ifs, line);
+  ss.clear();
+  ss.str(line.substr(4));
+  for (int row = 0; row < 3; ++row)
+  {
+    for (int col = 0; col < 4; ++col)
+    {
+      ss >> value;
+      calibration_data.P1(row, col) = value;
+    }
+  }
+
+  std::getline(ifs, line);
+  ss.clear();
+  ss.str(line.substr(4));
+  for (int row = 0; row < 3; ++row)
+  {
+    for (int col = 0; col < 4; ++col)
+    {
+      ss >> value;
+      calibration_data.P2(row, col) = value;
+    }
+  }
+
+  std::getline(ifs, line);
+  ss.clear();
+  ss.str(line.substr(4));
+  for (int row = 0; row < 3; ++row)
+  {
+    for (int col = 0; col < 4; ++col)
+    {
+      ss >> value;
+      calibration_data.P3(row, col) = value;
+    }
+  }
+
+  std::getline(ifs, line);
+  ss.clear();
+  ss.str(line.substr(9));
+  for (int row = 0; row < 3; ++row)
+  {
+    for (int col = 0; col < 3; ++col)
+    {
+      ss >> value;
+      calibration_data.R0_rect(row, col) = value;
+    }
+  }
+
+  std::getline(ifs, line);
+  ss.clear();
+  ss.str(line.substr(16));
+  for (int row = 0; row < 3; ++row)
+  {
+    for (int col = 0; col < 4; ++col)
+    {
+      ss >> value;
+      calibration_data.velo_to_cam(row, col) = value;
+    }
+  }
+
+  std::getline(ifs, line);
+  ss.clear();
+  ss.str(line.substr(16));
+  for (int row = 0; row < 3; ++row)
+  {
+    for (int col = 0; col < 4; ++col)
+    {
+      ss >> value;
+      calibration_data.imu_to_velo(row, col) = value;
+    }
+  }
+  ifs.close();
+  return 0;
+}
+
+bool Dataloader::load_kitti_label(
+  const std::string& label_path,
+  std::vector<kitti::Label>& labels)
+{
+  std::ifstream ifs(label_path);
+  std::string line;
+  if (!ifs.is_open())
+  {
+    std::cout << "cannot open label_path : " << label_path << std::endl;
+    return -1;
+  }
+  while (std::getline(ifs, line))
+  {
+    kitti::Label label;
+    std::stringstream ss(line);
+    ss >> label.type >> label.truncated >> label.occluded >> 
+    label.alpha >> label.left >> label.top >> label.right >> 
+    label.bottom >> label.height >> label.width >> label.length >> 
+    label.loc_x >> label.loc_y >> label.loc_z >> label.rot_y >> 
+    label.score;
+    labels.push_back(label);
+  }
+  ifs.close();
+  return 0;
+}
+
+bool Dataloader::load_kitti_image(
+  const std::string& image_path,
+  cv::Mat& image)
+{
+  image = cv::imread(image_path, cv::IMREAD_ANYCOLOR);
+  if (image.empty())
+  {
+    std::cout << "cannot load image from image_path : !" << image_path << std::endl;
+    return -1;
+  }
+  return 0;
+}
+
+kitti::Data Dataloader::get_kitti_data(const uint16_t frame_id)
+{
+  kitti::Data kitti_data;
+  std::cout << kitti_root_path_ << std::endl;
+  std::string str_sframe_id = std::to_string(frame_id);
+  std::string file_name = std::string(string_length_ - str_sframe_id.length(), '0') + str_sframe_id;
+  std::string calibration_file_path = kitti_root_path_ + 
+    calibration_path_ + file_name + ".txt";
+  std::string image_file_path = kitti_root_path_ + 
+    image_path_ + file_name + ".png";
+  std::string label_file_path = kitti_root_path_ + 
+    label_path_ + file_name + ".txt";
+  std::cout << calibration_file_path << std::endl;
+  load_kitti_calibration(calibration_file_path, kitti_data.calibration);
+  load_kitti_image(image_file_path, kitti_data.image);
+  load_kitti_label(label_file_path, kitti_data.labels);
+  return kitti_data;
+}
